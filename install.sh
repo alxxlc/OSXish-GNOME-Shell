5c0d824c30ae0ba985361f1818673f6f2d3aca2c
Alex Coté <alx929@gmail.com>
2018-03-02 13:44:28 -0600

    Added (unfinished) install script.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pre-reqs:
+#	pacman -S ruby-sass ruby-bundler
+
+echo "Setting application theme..."
+mkdir ~/.themes
+cp ./Gnome-OSX-V-HSierra/ ~/.themes
+
+echo "Setting shell theme..."
+
+echo "Setting system fonts..."
