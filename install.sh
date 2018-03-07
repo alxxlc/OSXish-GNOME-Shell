2cc74a36150677f6bbcf1f8ae07eec063775cfff
Alex Coté <alx929@gmail.com>
2018-03-06 21:41:06 -0600

    Built up the install script a little more.

--- install.sh
+++ install.sh
@@ -3,6 +3,24 @@
 # Pre-reqs:
 #	pacman -S ruby-sass ruby-bundler npm
 
+# Credits:
+# La Capitaine Icons:
+#	https://github.com/keeferrourke/la-capitaine-icon-theme
+#
+# Gnome-OSX Theme:
+#	https://www.opendesktop.org/p/1171688/
+#
+# Sierra GTK Theme:
+#	https://github.com/vinceliuice/Sierra-gtk-theme
+#
+# Lato Font:
+#	http://www.latofonts.com/lato-free-fonts/
+#	https://fonts.google.com/specimen/Lato
+
+echo -e "\e[96mBuilding theme files..."
+chmod +x ./Sierra-gtk-theme/Install
+./Sierra-gtk-theme/Install
+
 echo -e "\e[96mSetting application theme..."
 mkdir -p ~/.themes
 cp ./Gnome-OSX-V-HSierra/ ~/.themes
@@ -11,5 +29,10 @@ echo -e "\e[96mSetting shell theme..."
 
 echo -e "\e[96mSetting system fonts..."
 mkdir -p ~/.local/share/fonts/
+cp ./Lato/*.ttf ~/.local/share/fonts/
+# Set interface font
+gsettings set org.gnome.desktop.interface font-name "Lato Light 10.5"
+# Set window title font
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Lato Regular 11"
 
 echo -e "\e[96mTweaking GNOME..."
