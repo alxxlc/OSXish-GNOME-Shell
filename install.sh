9305941a44831946aef7e5b66c67b06e39c7c31e
Alex Coté <alx929@gmail.com>
2018-03-07 18:23:42 -0600

    Almost done with install script. Added non-Apple assets that are used for tweaking the theme.

--- install.sh
+++ install.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-# Pre-reqs:
-#	pacman -S ruby-sass ruby-bundler npm
-
 echo -e "\e[96mSetting application theme..."
 mkdir -p ~/.themes
-cp ./Gnome-OSX-V-HSierra/ ~/.themes
+cp -r ./Gnome-OSX-V-HSierra/ ~/.themes
 
 echo -e "\e[96mSetting shell theme..."
+chmod +x ./Sierra-gtk-theme/Install
+./Sierra-gtk-theme/Install
+
+echo -e "\e[96mTweaking shell theme..."
+# Remove cp alias, if any.
+# This is to prevent prompts for overwriting files.
+unalias cp
+cp -r ./AssetTweaks/assets/* ~/.themes/Sierra-light-solid/gnome-shell/assets/
+
+echo -e "\e[96mSetting icon theme..."
+mkdir -p ~/.icons
+cp -r ./la-capitaine-icon-theme/ ~/.icons
 
 echo -e "\e[96mSetting system fonts..."
 mkdir -p ~/.local/share/fonts/
+cp ./Lato/*.ttf ~/.local/share/fonts/
 
 echo -e "\e[96mTweaking GNOME..."
+### Set themes:
+# Set application theme
+gsettings set org.gnome.desktop.interface gtk-theme "Gnome-OSX-V-HSierra"
+# Set shell theme
+gsettings set org.gnome.shell.extensions.user-theme name "Sierra-light-solid"
+# Set icon theme
+gsettings set org.gnome.desktop.interface icon-theme "la-capitaine-icon-theme"
+# Set interface font
+gsettings set org.gnome.desktop.interface font-name "Lato Light 10.5"
+# Set window title font
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Lato Regular 11"
